c6d61c2f438f8165f24d891b7b0cec192590ac09
Churs <a.chursinov@heyworks.com>
2018-11-17 16:10:49 +0300

    #171. Add projectiles spowening system

--- Assets/Script/Projectile.cs
+++ Assets/Script/Projectile.cs
@@ -17,4 +17,12 @@ public class Projectile : MonoBehaviour {
         transform.Translate(Vector3.right * speed * Time.deltaTime);
 
 	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Shreder")
+        {
+            Destroy(gameObject);
+        }
+    }
 }
---
+++ Assets/Script/Shooter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+    public GameObject projectile;
+    public GameObject projectileParent;
+    public GameObject gun;
+
+    private void Fire()
+    {
+        GameObject newProjectile = Instantiate(projectile, gun.transform.position, gun.transform.rotation) as GameObject;
+        //newProjectile.
+    }
+
+    
+}
