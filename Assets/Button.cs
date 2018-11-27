6986bc7cff83b6a73d0456541889037ccd8da014
Churs <a.chursinov@heyworks.com>
2018-11-27 22:29:50 +0300

    #183. Add button panel

---
+++ Assets/Button.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    SpriteRenderer buttonSpriteRenderer;
+    
+    // Use this for initialization
+	void Start () {
+        buttonSpriteRenderer = GetComponent<SpriteRenderer>();
+        buttonSpriteRenderer.color = Color.black;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        Debug.Log(gameObject.name + " pressed");
+        buttonSpriteRenderer.color = Color.white;
+    }
+}
