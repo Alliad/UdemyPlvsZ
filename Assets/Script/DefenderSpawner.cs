f666f7edc0f39c6cf2acc43da668edcee4d9e97e
Churs <a.chursinov@heyworks.com>
2018-12-20 10:42:36 +0300

    Instanciate defender in point

--- Assets/Script/DefenderSpawner.cs
+++ Assets/Script/DefenderSpawner.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class DefenderSpawner : MonoBehaviour {
 
-	// Use this for initialization
+    private Camera camera;
+    
+    // Use this for initialization
 	void Start () {
-		
+        camera = Camera.main;
 	}
 	
 	// Update is called once per frame
@@ -15,13 +17,22 @@ public class DefenderSpawner : MonoBehaviour {
 	}
 
     private void OnMouseDown()
-    {
-        Debug.Log(Input.mousePosition);
-        Debug.Log(CalculateWorldPointsOfMouseClick());
+    {        
+        Debug.Log(SnapToGrid(CalculateWorldPointsOfMouseClick(Input.mousePosition)));
+        Instantiate(Button.selectedDefender, SnapToGrid(CalculateWorldPointsOfMouseClick(Input.mousePosition)), Quaternion.identity);
     }
 
-    Vector2 CalculateWorldPointsOfMouseClick()
+    Vector2 SnapToGrid(Vector2 mouseClickInWorldUnitsPosition)
     {
-        return new Vector2(0, 0);
+        return new Vector2(Mathf.Round(mouseClickInWorldUnitsPosition.x), Mathf.Round(mouseClickInWorldUnitsPosition.y));
+    }
+
+    Vector2 CalculateWorldPointsOfMouseClick(Vector3 mousePosition)
+    {
+        //var v3 = camera.ScreenToWorldPoint(mousePosition);
+        Vector2 v2 = camera.ScreenToWorldPoint(mousePosition);
+        //v2 = v3;
+        //Debug.Log("X cordinate " + Mathf.Round(v2.x) + " Y cordinate " + Mathf.Round(v2.y));
+        return v2;
     }
 }
