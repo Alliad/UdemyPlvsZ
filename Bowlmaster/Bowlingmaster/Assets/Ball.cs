418ad136c6aedc982581e737ad13d6beeb32ae01
Churs <5016105@gmail.com>
2019-01-24 02:22:37 +0300

    #244

---
+++ Bowlmaster/Bowlingmaster/Assets/Ball.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Ball : MonoBehaviour {
+
+    private Rigidbody ballRB;
+    private AudioSource myAudioSource;
+    [SerializeField] private float impulse;
+    
+    
+    
+    // Use this for initialization
+	void Start () {
+
+        ballRB = GetComponent<Rigidbody>();
+        myAudioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ballRB.AddForce(0, 0, impulse, ForceMode.Impulse);
+            myAudioSource.Play();
+        }
+	}
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Floar")
+        {
+            myAudioSource.Play();
+        }
+    }*/
+}
---
+++ Bowlmaster/Bowlingmaster/Assets/CameraControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    [SerializeField] private GameObject ball;
+
+    // Offset betwine main camera and the ball
+    private Vector3 offset;
+    private Vector3 cameraPosition;
+    private float stopPozition = 1800; 
+    
+    // Use this for initialization
+	void Start () {
+        offset = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y, ball.transform.position.z - gameObject.transform.position.z);
+        Debug.Log(offset);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (ball.transform.position.z <= stopPozition)
+        {
+            gameObject.transform.position = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y, ball.transform.position.z - offset.z);
+        }
+        
+
+
+    }
+}
