f0c346ab97ce6cc4a02bd0e9816a25a94ed8624f
Churs <a.chursinov@heyworks.com>
2019-01-30 22:29:24 +0300

    #253 Add swiper animation & pin's state detection

---
+++ Bowlmaster/Bowlingmaster/Assets/Scripts/Pin.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    [SerializeField] private float standingThreshold = 3f;
+    
+    
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(name + IsStanding());
+    }
+
+    public bool IsStanding()
+    {
+        if (Mathf.Abs (transform.eulerAngles.x) > standingThreshold || 
+            Mathf.Abs (transform.eulerAngles.z) > standingThreshold)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+}
