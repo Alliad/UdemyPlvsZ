f3508b8fd7db459e789ef078dafb9bebc562e3f8
Churs <a.chursinov@heyworks.com>
2019-02-03 18:21:36 +0300

    #195 add standing pins counter

---
+++ Bowlmaster/Bowlingmaster/Assets/Scripts/PinSetter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinSetter : MonoBehaviour
+{
+    private Pin[] pinsArray;
+    private int standingPinsCount;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pinsArray = FindObjectsOfType<Pin>();
+        standingPinsCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("STanding pins count " + CountStandingPins());
+    }
+
+    public int CountStandingPins()
+    {
+        for (int i = 0; i < pinsArray.Length; i++)
+        {
+            if (pinsArray[i].IsStanding())
+            {
+                standingPinsCount++;
+            }
+        }
+        return standingPinsCount;
+    }
+}
